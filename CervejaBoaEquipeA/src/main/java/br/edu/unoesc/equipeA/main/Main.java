560df22129e96793d58532f215a27bfded52903e
Luiz Felipe <Luiz Felipe@LFELIPE>
2016-11-02 22:51:54 -0200

    Deem Pull e vejam o ícone na barra de tarefas e aplicativo.

--- CervejaBoaEquipeA/src/main/java/br/edu/unoesc/equipeA/main/Main.java
+++ CervejaBoaEquipeA/src/main/java/br/edu/unoesc/equipeA/main/Main.java
@@ -3,6 +3,7 @@ package br.edu.unoesc.equipeA.main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -16,6 +17,8 @@ public class Main extends Application {
 			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource(PATH_VIEW + "login.fxml"));
 			Scene scene = new Scene(root);
 			// scene.getStylesheets().add("/css/style.css");
+			primaryStage.getIcons().add(new Image("/icon/beer-icon.png"));
+			primaryStage.setTitle("Cerveja Boa - Equipe A");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (Exception e) {
